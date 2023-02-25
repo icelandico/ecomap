626220835c6cf7b255bd90e1164ba48256d94502
Michal <hi@geodev.me>
2023-02-26 00:29:30 +0100

    feat: create login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,30 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Text } from "@mantine/core";
+import { Input, Text, Title } from "@mantine/core";
+import { IconAt, IconLock } from "@tabler/icons-react";
 
 import { api } from "@/utils/api";
 import { Button } from "@mantine/core";
 
 const Login: NextPage = () => {
-    return (
-        <Text>LOGIN</Text>
-    );
+  return (
+    <div className="m-0 m-auto flex w-1/5 flex-col items-center justify-center">
+      <Title className="mb-4" order={2}>
+        Login
+      </Title>
+      <Input
+        className="mb-4 w-full"
+        icon={<IconAt />}
+        placeholder="Your email"
+      />
+      <Input
+        className="mb-4 w-full"
+        icon={<IconLock />}
+        placeholder="Your password"
+      />
+      <Button className="mt-6 w-full">Next</Button>
+    </div>
+  );
 };
 
 export default Login;
